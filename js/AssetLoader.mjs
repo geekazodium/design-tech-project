b15e39207bb73ff9b720b435437ef2630cbcc8f2
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2023-04-19 14:40:51 +0800

    Edited method of loading assets

--- js/AssetLoader.mjs
+++ js/AssetLoader.mjs
@@ -1,6 +1,28 @@
 class AssetLoader{
     constructor(path){ 
-        this.path = path
+        this.path = path;
+        this.loaders = new Map();
+        this.initImgLoader();
+        this.initBBMLoader();
+        this.bbModels = [];
+    }
+    initImgLoader(){
+        this.loaders.set("img",(element)=>{
+            var img = new Image(element.width,element.height);
+            img.src = element.src;
+            img.id = element.id;
+            document.body.appendChild(img);
+        });
+    }
+    initBBMLoader(){
+        this.loaders.set("bbModel",(element)=>{
+            fetch(this.path)
+                .then((response) => response.json())
+                .then((json) => {this.json = json;})
+                .then(() => {
+                    this.bbModels.push(this.json);
+                });
+        });
     }
     load(res){
         this.json = undefined;
@@ -9,15 +31,15 @@ class AssetLoader{
             .then((json) => {this.json = json;})
             .then(() => {
                 this.json.forEach(element => {
-                    if(element.type == "img"){
-                        var img = new Image(element.width,element.height);
-                        img.src = element.src;
-                        img.id = element.id;
-                        document.body.appendChild(img);
-                    }
+                    this.loadItem(element);
                 });
                 res();
             });
+    }
+    async loadItem(element){
+        var loader = this.loaders.get(element.type);
+        if(loader == undefined) return;
+        loader(element);
     }
 }
 
