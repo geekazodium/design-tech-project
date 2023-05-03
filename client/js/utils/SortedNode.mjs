754bf225c3236d8ca430cb52f73fdbe09f58cde5
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2023-05-03 11:45:53 +0800

    Created function implementation of quicksort

--- client/js/utils/SortedNode.mjs
+++ client/js/utils/SortedNode.mjs
@@ -53,4 +53,51 @@ class SortedNode{
     }
 }
 
-export {SortedNode};
+function quickSort(section,compareFunction){
+    var item
+    if(section.length < 1){
+        throw new Error("possibly flawed compare function, this should not be possible");
+    }else if(section.length == 1){
+        item = section[0];
+        return item;
+    }
+
+    let length = section.length;
+    let half = length>>1;
+    item = section[half];
+
+    let before = new Array();
+    let after = new Array();
+
+    for(let i = 0; i<length; i++){
+        if(i == half)continue;
+
+        let element = section[i];
+        if(compareFunction(item,element)){
+            after.push(element);
+        }else{
+            before.push(element);
+        }
+    }
+    
+    let ret = new Array();
+    if(before.length>0){
+        ret = ret.concat(quickSort(before,compareFunction));
+    }
+    ret = ret.concat(item);
+    if(after.length>0){
+        ret = ret.concat(quickSort(after,compareFunction));
+    }
+    if(item == undefined) return [];
+    return ret;
+}
+
+export {SortedNode,quickSort};
+
+
+function sortBenchmark(list, sortMethod, compareFunction){
+    var start = +new Date();
+    var sorted = sortMethod(list, compareFunction);
+    var end = +new Date();
+    return [sorted, end-start];
+}
