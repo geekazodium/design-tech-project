c3f61b4ae7d4236b69bccc70f7e16f78e6129e0c
Geekazodium <59382423+geekazodium@users.noreply.github.com>
2023-05-09 21:03:28 +0800

    Enabled mipmapping

--- client/js/render/TerrainRenderer.mjs
+++ client/js/render/TerrainRenderer.mjs
@@ -94,7 +94,7 @@ class TerrainRenderer extends Renderer{
             totalTextures
         );
 
-        gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
         gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
         gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
@@ -135,6 +135,7 @@ class TerrainRenderer extends Renderer{
                 0
             );
         }
+        gl.generateMipmap(gl.TEXTURE_2D_ARRAY);
     }
     /**
      * 
